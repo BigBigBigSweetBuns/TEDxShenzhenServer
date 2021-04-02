4c768a0852792e096f39930da0914ae92bfb1515
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-03 00:13:29 +0800

    add authors data

---
+++ data/authors.js
@@ -0,0 +1,7 @@
+const structure = {
+    id: 0,                // 作者ID
+    name: "name",         // 作者姓名/昵称
+    imgSrc: "/img.jpg",   // 作者头像 相对路径
+    path: "/author",      // 前端页面访问，路由索引
+    articleList: [0],     // 作者写的文章列表， item 为文章ID
+}
