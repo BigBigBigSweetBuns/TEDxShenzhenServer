23d7b6b26d2e046d0b633bbc00c820698b755cfa
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-18 21:37:06 +0800

    update video  type

--- data/videos.js
+++ data/videos.js
@@ -1,4 +1,5 @@
 const ossThumbnailBaseURL = "https://tedxshenzhen.oss-cn-shenzhen.aliyuncs.com/videos/thumbnail"
+const ossVideoBaseURL = "https://tedxshenzhen.oss-cn-shenzhen.aliyuncs.com/videos/"
 // 数据结构
 const structure = {
     id: 0,                       // 文章ID
@@ -29,8 +30,8 @@ const data = [
         introduction: "选择性缄默症患者在沉默中忍受痛苦。他们常常被忽视而得不到治疗，尤其是在像中国这样的发展中国家。",
         contentHTML: "<p>选择性缄默症患者在沉默中忍受痛苦。他们常常被忽视而得不到治疗，尤其是在像中国这样的发展中国家。提高对这种焦虑症的认识至关重要，因为在采取行动帮助患者之前，我们首先需要了解这种病症。心理学家露丝·佩雷德尼克（Ruth Perednik）一生都专注于帮助患有选择性缄默症的儿童找到他们自己的声音。</p><p>Ruth Perednik，英国出生的以色列儿童心理学家，选择性缄默症领域的先驱。在过去20年中，她一直专注于治疗选择性缄默症和其他焦虑症患儿，并开发了一种基于认知行为学的选择性缄默症治疗方法，成功地帮助了</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=92253943&bvid=BV1m7411N79n&cid=157428581&page=1",
+            type: "src",
+            src: "/2019/why-they-can-only-be-silent-to-strangers.mp4",
         },
         division: "talk",
         createTS: "",
@@ -46,8 +47,8 @@ const data = [
         introduction: "都说基础教育要改革，孩子的教育要创新。但怎样的方向更适合现在的孩子呢？",
         contentHTML: "<p>都说基础教育要改革，孩子的教育要创新。但怎样的方向更适合现在的孩子呢？葛庆飞老师给我们来带了一个全科融合教学的新视角。</p><p>葛庆飞10年来投身基础教育研究，针对现在教育的弊端，独创了一套全新的教学理念，经过上百孩子的配合测试，逐渐形成了覆盖从小学到高三的全套全科融合式教学体系。其创新教育的理念近期开始引起多家媒体的关注，并成功入围央视逐梦年代栏目，成为中国企业品牌创新的代表。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93021353&bvid=BV11E411E7DZ&cid=158712044&page=1",
+            type: "src",
+            src: "/2019/how-do-we-need-to-rethink-our-basic-education.mp4",
         },
         division: "talk",
         createTS: "",
@@ -63,8 +64,8 @@ const data = [
         introduction: "研究美国国家步道对于美国特别是美国青年人的影响、朝圣之路对于欧洲的重要影响，以及西南联大的迁徙对于学生成长产生的深刻影响",
         contentHTML: "<p>他用27 年时间，走过中国31个省市自治区，在大部分省待了超过一个月时间。自2011年至今，带领上万人以各种形式行走在中国大地上。他研究了美国国家步道对于美国特别是美国青年人的影响、朝圣之路对于欧洲的重要影响，以及西南联大的迁徙对于学生成长产生的深刻影响，希望陪伴青年人通过止语徒步中感受我们的国家。</p><p>半落，曾为海军航空兵军官，在北海舰队、东海舰队服役。现为究径止语徒步创始人，在国内开辟了24条9-10天的止语徒步线路，结合重要历史文化节点，陪伴中国年轻人在行走中了解自己，感受中华。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93272539&bvid=BV1hE411771M&cid=159227228&page=1",
+            type: "src",
+            src: "/2019/feel-the-power-of-the-land-and-self-by-walking.mp4",
         },
         division: "talk",
         createTS: "",
@@ -80,8 +81,8 @@ const data = [
         introduction: "就像大多数的“天才”设定，谷逍驰在剑桥大学的时候辍学并带领团队，探索着下一代的计算机技术：为我们的“虚拟世界”创造“真实”",
         contentHTML: "<p>就像大多数的“天才”设定，谷逍驰在剑桥大学的时候辍学并带领团队，探索着下一代的计算机技术：为我们的“虚拟世界”创造“真实”——开发出了全球第一套无线力反馈手套系统。带上手套，我们可以在家里的沙发上学习做饭、在虚拟的游戏空间里划桨冲浪…</p><p>谷逍驰是Dexta Robotics 的创始人兼 CEO，个人拥有数十项全球范围专利，现专注于构建下一代基于空间计算的通讯及交互系统。他曾入选由Facebook的天使投资人Peter Thiel遴选的泰尔奖学金20位20岁以下创业者；在22岁被授</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93358562&bvid=BV1cE4114798&cid=159331430&page=1",
+            type: "src",
+            src: "/2019/when-creation-becomes-a-habit.mp4",
         },
         division: "talk",
         createTS: "",
@@ -97,8 +98,8 @@ const data = [
         introduction: "你以为的“碳排放”只来自工业和汽车？原来农业也会产生“碳排放”！",
         contentHTML: "<p>你以为的“碳排放”只来自工业和汽车？原来农业也会产生“碳排放”！如何解决这个当今全球性的环境议题呢？除了大众了解的减能减排，韩农和他的科技团队，花了十年的时间致力于研究和实践如何让土壤微生物来帮忙减少碳排放。</p><p>韩农，有机农业专家，特石生态循环有机农业产业化模式创始人，作为中国最早从事生态有机农业系统性研究和实践的企业家之一。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93358912&bvid=BV1wE41147Nc&cid=159366144&page=1",
+            type: "src",
+            src: "/2019/reduce-carbon-emissions.mp4",
         },
         division: "talk",
         createTS: "",
@@ -114,8 +115,8 @@ const data = [
         introduction: "洞穴是什么？是另一个没有光的“星球”。",
         contentHTML: "<p>洞穴是什么？是另一个没有光的“星球”。中国探洞数量最多的探洞者刘佳将带领我们感受了洞穴下的世界，告诉我们，人类与洞穴之间还能有怎么样的联系？</p><p>刘佳被誉为是“当代徐霞客”，从2004年至今的16年间，一直潜行在隐秘的洞穴世界，如今已成长为中国地穴探险的先锋，中国探洞界一姐。她是2016美国国家地理年度探险人物评选中国区十大提名探险家之一，中国第二深竖洞纪录保持者。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93418974&bvid=BV1gE41147ki&cid=159471085&page=1",
+            type: "src",
+            src: "/2019/the-cave-is-a-planet-without-light.mp4",
         },
         division: "talk",
         createTS: "",
@@ -131,8 +132,8 @@ const data = [
         introduction: "“城市更新”是城市现代化过程中一定面临的议题。",
         contentHTML: "<p>“城市更新”是城市现代化过程中一定面临的议题。一边是城市的记忆和温情，一边是城市高速发展的经济和功能需求，这两者就一定矛盾吗？鱼和熊掌是否能兼得？</p><p>张之杨，独立建筑师，深圳“局内设计(Ingame office)”创始人/主创设计师。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93577452&bvid=BV1FE411t7xR&cid=159759636&page=1",
+            type: "src",
+            src: "/2019/building-city-Developmental-self-contradiction.mp4",
         },
         division: "talk",
         createTS: "",
@@ -182,8 +183,8 @@ const data = [
         introduction: "在意识到需要改变自己的20岁，她用全部家当自主学习完成软件编程、掌握英语能力...",
         contentHTML: "<p>媒体称孙玲为“励志网红”，出生成长于湖南娄底一个小村镇的她，12岁经历初中被迫辍学、19岁高考失利，当过理发学徒、工厂流水线生产女工。</p><p>在意识到需要改变自己的20岁，她用全部家当自主学习完成软件编程、掌握英语能力，在10年时间的不懈努力之下，她成功申请到美国留学，现作为程序员在美国Google办公室工作。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93418742&bvid=BV1gE41147BX&cid=159812600&page=1",
+            type: "src",
+            src: "/2019/assembly-line-workers-become-New-York-programmers.mp4",
         },
         division: "talk",
         createTS: "",
@@ -199,8 +200,8 @@ const data = [
         introduction: "研究了7年环保事业的雒晓彬说，事实上我们每周在无形当中都吃进去了一张信用卡大小的塑料。",
         contentHTML: "<p>研究了7年环保事业的雒晓彬说，事实上我们每周在无形当中都吃进去了一张信用卡大小的塑料。而面对塑料对我们的不断侵蚀，雒晓彬将告诉我们该如何反抗。</p><p>雒晓彬，深圳蜗牛妈妈科技有限公司创始人，拥有六年以上世界500强上市公司高管经历，从事环保NGO工作七年，环太平洋⽆塑星球联盟创始⼈之一，被大家称为“蜗牛哥”。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93614747&bvid=BV18E411s7kw&cid=159820594&page=1",
+            type: "src",
+            src: "/2019/how-much-plastic-do-we-eat-every-week.mp4",
         },
         division: "talk",
         createTS: "",
@@ -216,8 +217,8 @@ const data = [
         introduction: "一位右臂截肢的少女为何能用双手在钢琴上弹奏出美妙的歌曲？",
         contentHTML: "<p>一位右臂截肢的少女为何能用双手在钢琴上弹奏出美妙的歌曲？</p><p>答案是“脑机接口”。全球脑机接口领域五位创新者之一的韩壁丞博士将介绍如何将脑机接口技术用于帮助残障人士，并在未来能够帮助更多人恢复社会功能。</p><p>韩璧丞的脑机接口公司BrainCo、新型脑控义肢公司BrainRobotics，在美国评选的世界十大脑机接口公司中占据两席重地。</p><p>他与特斯拉的创始人伊隆·马斯克（Elon Musk）一同被美国媒体评为脑机接口领域五位创新者之一 ，2017年被百年科技杂志MIT《麻省理工科技评论》评选为</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93616248&bvid=BV1bE411s7Zi&cid=159819840&page=1",
+            type: "src",
+            src: "/2019/help-people-with-disabilities-return-to-life.mp4",
         },
         division: "talk",
         createTS: "",
@@ -233,8 +234,8 @@ const data = [
         introduction: "是什么让你的食物变得独一无二？不是你的家乡，也不是你的文化，而是对于食物的...",
         contentHTML: "<p>是什么让你的食物变得独一无二？不是你的家乡，也不是你的文化，而是对于食物的热爱，以及尝试的勇气，让食物变得独一无二。在美食家罗朗来看，美食和其他艺术形式一样，都能起到连接人心的作用。</p><p>罗朗，主厨，作家，环保主义者。美国驻中国大使馆两任大使的行政总厨及官邸总管, 《美食与美酒》杂志2016年中国100名最具影响力的人物之一。</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93419120&bvid=BV1gE41147SX&cid=159819167&page=1",
+            type: "src",
+            src: "/2019/what-is-the-unique-recipe-for-gourmet-food.mp4",
         },
         division: "talk",
         createTS: "",
@@ -243,20 +244,20 @@ const data = [
     }, {
         id: 15,
         thumbnail: {
-            src: "/happy-Architecture.jpg",
+            src: "/happy-architecture.jpg",
             alt: "thumbnail",
         },
         title: "把建筑空间变成巨型玩具——快乐建筑学 | 阮文韬Manfred Yuen",
         introduction: "阮文韬意识到，作为建筑师，我们不应该只关注于冰冷的建筑形式，材料和规模，更应该致力...",
         contentHTML: "<p>从世界级事务所到回归香港本土，通过一系列志愿者的工作——为单身老人修缮公屋，为要被消灭的“排档”去说服政府……阮文韬意识到，作为建筑师，我们不应该只关注于冰冷的建筑形式，材料和规模，更应该致力于关注和创造建筑可以提供的东西，就是快乐和幸福。</p><p>阮文韬，GROUNDWORK元新建城创办人，阮文韬具有多重身份——建筑师、专栏作家、电台节目嘉宾主持、大学讲师。而在多重身份的构成下，他执着追求设计的梦想和敢为人先的精神，他的设计结合了人文与美学带给建筑不一样的生命。</p><p>自2007年创办Groun</p>",
         video: {
-            type: "iframe",
-            src: "//player.bilibili.com/player.html?aid=93419120&bvid=BV1gE41147SX&cid=159819167&page=1",
+            type: "src",
+            src: "/2019/happy-architecture.mp4",
         },
         division: "talk",
         createTS: "",
         authorID: 0,
-        path: "/happy-Architecture",
+        path: "/happy-architecture",
     }, {
         id: 16,
         thumbnail: {
@@ -281,6 +282,9 @@ function videosInit() {
     for (let i = 0; i < data.length; i++) { // 将src补全
         output.push(data[i]);
         output[i].thumbnail.src = ossThumbnailBaseURL + data[i].thumbnail.src;
+        if (data[i].video.type == "src") {
+            output[i].video.src = ossVideoBaseURL + data[i].video.src;
+        }
     }
     return output;
 }
