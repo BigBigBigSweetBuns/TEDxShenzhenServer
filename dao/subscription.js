f94df3a94b1f71b31c0f4e3cce2ad4735a5c5fbc
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-24 21:01:47 +0800

    update tools  subscription and add unsubscription

--- dao/subscription.js
+++ dao/subscription.js
@@ -1,10 +1,5 @@
 const fs = require("fs");
-function getDate() {
-    let myDate = new Date();
-    let month = myDate.getMonth() > 9 ? myDate.getMonth() + 1 : "0" + (myDate.getMonth() + 1);
-    let date = myDate.getDate() > 9 ? myDate.getDate() : "0" + myDate.getDate();
-    return myDate.getFullYear().toString() + month.toString() + date.toString();
-}
+
 const subscriptionDAO = function () {
     this.select = function (file) {
         fs.readFile(file, 'utf-8', function (err, data) {
@@ -15,14 +10,13 @@ const subscriptionDAO = function () {
                 return data;
         })
     };
-    this.write = function (data) {
-        let file = "./" + getDate() + ".txt";
+    this.write = function (data, file, callback) {
         fs.writeFile(file, data + "\n", { 'flag': 'a' }, (err) => {
-            console.log("hello")
             if (err) {
-                return "err";
+                console.log(err);
+                callback(false)
             }
-            return "ok";
+            callback(true)
         })
     }
 }
--- routes/tools.js
+++ routes/tools.js
@@ -4,6 +4,13 @@ const Dao = require("../dao/subscription.js");
 const subscriptionDAO = new Dao;
 const fs = require("fs");
 
+function getDate() {
+    let myDate = new Date();
+    let month = myDate.getMonth() > 9 ? myDate.getMonth() + 1 : "0" + (myDate.getMonth() + 1);
+    let date = myDate.getDate() > 9 ? myDate.getDate() : "0" + myDate.getDate();
+    return myDate.getFullYear().toString() + month.toString() + date.toString();
+}
+
 const returnFilter = function (boolean) {
     if (boolean)
         return {
@@ -20,15 +27,25 @@ const returnFilter = function (boolean) {
 router.post('/subscription', function (req, res, next) {
     //todo fs为异步请求需要调整
     const params = req.body;
-    subscriptionDAO.write(params.email);
-    res.json(returnFilter(true));
-})
-router.post("/download", function (req, res, next) {
-    res.set({
-        "Content-Type": "application/octet-stream",//告诉浏览器这是一个二进制文件
-        "Content-Disposition": "attachment; filename=1.txt"//告诉浏览器这是一个需要下载的文件
+    const file = "/srv/emailFile/subscription/" + getDate() + ".txt";
+    subscriptionDAO.write(params.email, file, (r) => {
+        res.json(returnFilter(r))
     });
-    fs.createReadStream('./20210423.txt').pipe(res);
 })
+router.post('/unsubscription', function (req, res, next) {
+    //todo fs为异步请求需要调整
+    const params = req.body;
+    const file = "/srv/emailFile/unsubscription/" + getDate() + ".txt";
+    subscriptionDAO.write(params.email, file, (r) => {
+        res.json(returnFilter(r))
+    });
+})
+// router.post("/download", function (req, res, next) {
+//     res.set({
+//         "Content-Type": "application/octet-stream",//告诉浏览器这是一个二进制文件
+//         "Content-Disposition": "attachment; filename=1.txt"//告诉浏览器这是一个需要下载的文件
+//     });
+//     fs.createReadStream('./20210423.txt').pipe(res);
+// })
 
 module.exports = router;
